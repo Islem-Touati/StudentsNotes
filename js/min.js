089452c9b43bfd75c7442b944b0aa201c0c6916f
Lokmani-ibrahim <148336289+Islem-Touati@users.noreply.github.com>
2023-11-14 11:38:40 +0100

    test full name valid ou no
    test if
    -input full name vide
    -length between 3 and 20
    -format maj et min et espace

--- js/min.js
+++ js/min.js
@@ -9,3 +9,22 @@ const task3 = document.querySelector('.task3');
 const result = document.querySelector('.result');
 const submit = document.querySelector('.submit');
 const reset = document.querySelector('.reset');
+//validation rules fullName
+
+//input full name
+function validateFullName(fullName){
+    //check if the full name is required.
+    if(!fullName){
+        return "Full Name is required .";
+    }
+    //check the length of the full name.
+    if(fullName.lenght < 3 || fullName.lenght > 20){
+        return "Full Name must be between 3 and 20 characters long.";
+    }
+    //check the format of the full name.
+    if(!/^[a-zA-Z]+$/.test(fullName)){
+        return "Full Name must contain only letters and spaces";
+    }
+    //if the full name passes all of the validations rules , return null.
+    return null;
+}
