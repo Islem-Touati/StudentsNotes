ef49556389ec555e81e786cc2ff19ca3d74fe30d
Lokmani-ibrahim <148336289+Islem-Touati@users.noreply.github.com>
2023-11-14 10:50:26 +0100

    add a basic js code
    récupérer les valeurs des champs du formulaire et les bouttons

---
+++ js/min.js
@@ -0,0 +1,11 @@
+// querySelector est une methode pour afficher le contenu de l input (récupérer les valeurs des champs du formulaire)
+const fullName = document.querySelector('.fullName');
+const dateOfBirth = document.querySelector('.dateOfBirth');
+const gender = document.querySelector('select[name="gender"]');
+const task1 = document.querySelector('.task1');
+const task2 = document.querySelector('.task2');
+const task3 = document.querySelector('.task3');
+//récupérer les bouttons
+const result = document.querySelector('.result');
+const submit = document.querySelector('.submit');
+const reset = document.querySelector('.reset');
