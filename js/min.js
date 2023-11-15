12d281145afb59ec699c583c6ca6f664f0cb75e9
Lokmani-ibrahim <148336289+Islem-Touati@users.noreply.github.com>
2023-11-15 12:04:19 +0100

    add input Select
    check if :
    -gender is required.
    -gender is a valid value
    -other

--- js/min.js
+++ js/min.js
@@ -14,15 +14,15 @@ const reset = document.querySelector('.reset');
 //input full name
 function validateFullName(fullName){
     //check if the full name is required.
-    if(!fullName){
+    if (!fullName){
         return "Full Name is required .";
     }
     //check the length of the full name.
-    if(fullName.lenght < 3 || fullName.lenght > 20){
+    if (fullName.length < 3 || fullName.lenght > 20){
         return "Full Name must be between 3 and 20 characters long.";
     }
     //check the format of the full name.
-    if(!/^[a-zA-Z]+$/.test(fullName)){
+    if (!/^[a-zA-Z]+$/.test(fullName)){
         return "Full Name must contain only letters and spaces";
     }
     //if the full name passes all of the validations rules , return null.
@@ -36,29 +36,37 @@ if (!date){
     return "date is required";
 }
 //check the format of the date.
-if(!/^d{4}-\d{2}$/ .test(date)){
-    return "date must be in the format YYYY/MM/DD";
+if (!/^d{2}-\d{4}$/ .test(date)){
+    return "date must be in the format DD/MM/YYYY";
 }
 //check the range of the date
 const today = new Date();
-const minDate = new Date ( today .getFullYear(), today.getMonth(), today.getDate());
-const maxDate = new Date(today.getFullYear() + 1, today.getMonth(), today.getDate());
+const minDate = new Date (  today.getMonth(), today.getDate(), today .getFullYear());
+const maxDate = new Date( today.getMonth(), today.getDate(), today.getFullYear() + 1);
 
-if(date < minDate || date > maxDate){
-    return "Date must be between today and one year from today.";
-}
-//if the date passes all the validation rules , return null.
+if (date < minDate || date > maxDate){
+    return "Date must be between today and one year from today.";}
+    //if the date passes all the validation rules , return null.
 return null;
 }
-
-
 //Afficher les valeurs des champs.
 submit.addEventListener('click', (event)=>{
-    console.log(fullName.value)
-    console.log(dateOfBirth.value)
-    console.log(task1.value)
-    console.log(task2.value)
-    console.log(task3.value)
-})
+if (validateFullName(fullName.value) !== null){
+    alert(validateFullName(fullName.value))
+    return;
+}})
 
 
+function validateGender(gender){
+    //check if the gender is required.
+    if (!gender){
+        return "Gender is required"
+    }
+    //check if the gender is a valid value.
+    const validGenders = ["Male" , "Female"];
+    if (!validGenders.includes(gender)){
+        return "Gender must be a valid value.";
+    }
+    //if the gender passes all of the validation rules, return null.
+    return null;
+}
